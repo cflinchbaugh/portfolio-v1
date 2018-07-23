1e5e347fab79b460867bc5a6dfacbb0d6febf67c
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2018-07-22 21:41:18 -0400

    Hello, World!

---
+++ portfolio/src/containers/Home.js
@@ -0,0 +1,14 @@
+import React, {Component, PropTypes} from 'react';
+
+class Home extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, World!</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;

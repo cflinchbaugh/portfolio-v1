daaccfeb210fe1a87fcd9f317a98130240b00a6b
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2019-05-05 17:44:53 -0400

    Populated Skills sectin

--- src/containers/Skills.js
+++ src/containers/Skills.js
@@ -7,6 +7,16 @@ const StyleWrapper = styled.section`
     align-items: center;
     justify-content: center;
     height: 100vh;
+
+    background: linear-gradient(to bottom, #fe8a7540 0%, #B5CBED 100%);
+
+    .heading {
+        font-size: 1.25em;
+    }
+
+    .skill {
+        margin: 10px;
+    }
     
 `
 
@@ -22,12 +32,20 @@ class Skills extends React.Component {
     render() {
         return (
             <StyleWrapper>
-                <div>
-                    Fresh Eyes: Reviewing content without preconceptions
+                <div className="heading">
+                    What sets me apart?
+                </div>
+                
+                <div className="skill">
+                    Communication: I specialize in <strong>people</strong>.  Identifying and communicating core messages to intended internal and external audiences every day is my mission.
                 </div>
 
-                <div>
-                    Focus: Solving the root problem, identifying process gaps
+                <div className="skill">
+                    Fresh Eyes: Reviewing content without preconceptions is essential to not missing the forest for the trees. Ensuring the best solutions are implemented, not just the most convenient, is my strategy.
+                </div>
+
+                <div className="skill">
+                    Focus: Solving the root problem, identifying process gaps, and ensuring consistency is my drive.
                 </div>
                 
 
